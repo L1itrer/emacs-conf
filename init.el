34364f6301ea6fdc2044a6300a247b58bf0ac617
Litrer <106829230+L1itrer@users.noreply.github.com>
2026-04-09 00:09:33 +0200

    More comments

--- init.el
+++ init.el
@@ -1,14 +1,23 @@
-(tool-bar-mode -1)
-(set-fringe-mode 10)
-(setq visible-bell t)
-(menu-bar-mode -1)
-(column-number-mode 1)
-;(scroll-bar-mode -1)
+;;; Basics
+(tool-bar-mode -1) ; nuke the tool bar
+(menu-bar-mode -1) ; nuke the menu bar
+;(scroll-bar-mode -1) ; nuke the scroll bar
 
+
+(setq visible-bell t) ; disable annoying sound on windows
+(set-fringe-mode 10) ; some space for scrolling
+
+;;; Ido mode (hints in minibuffer)
 (ido-mode 1)
 (ido-everywhere 1)
+(ido-ubiquitous-mode 1)
 
+; Make esc act as <C-g>
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+; Set some dark theme
 (load-theme 'wombat)
 
+;;; Line and column numbers
 (setq display-line-numbers 'relative)
+(column-number-mode 1)
