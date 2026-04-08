a7c1608e80f9dc676acff2f1131fc7dafd332212
Litrer <106829230+L1itrer@users.noreply.github.com>
2026-04-08 23:30:00 +0200

    Ready? Set? GO!

---
+++ init.el
@@ -0,0 +1,8 @@
+(tool-bar-mode -1)
+(set-fringe-mode 10)
+(setq visible-bell t)
+(menu-bar-mode -1)
+;(scroll-bar-mode -1)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(load-theme 'wombat)
