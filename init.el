ca141de7d5389ca1cedc79ed0a439d827b2eaf3e
Litrer <106829230+L1itrer@users.noreply.github.com>
2026-04-09 00:01:12 +0200

    Ido mode, relative line numbers

--- init.el
+++ init.el
@@ -2,7 +2,13 @@
 (set-fringe-mode 10)
 (setq visible-bell t)
 (menu-bar-mode -1)
+(column-number-mode 1)
 ;(scroll-bar-mode -1)
+
+(ido-mode 1)
+(ido-everywhere 1)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (load-theme 'wombat)
+
+(setq display-line-numbers 'relative)
